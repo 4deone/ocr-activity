3a487d804d8da53b6f11878d111027f0ba6869c6
by deone <playbydeone@gmail.com>
2018-10-17 11:22:22 +0100

    Ajout de la profession de l'utilisateur

--- monessaijs.js
+++ monessaijs.js
@@ -3,4 +3,6 @@ var nom = prompt("Entrez votre nom :");
 alert("Bonjour, " + prenom + " " + nom);
 var age = Number(prompt("Quel est votre age :"));
 alert("Vous avez " + age + " ans");
+var profession = prompt("Entrez votre profession");
+alert("Vous exercez en tant que " + profession);
 
