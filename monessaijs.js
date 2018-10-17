ba5c7077a07bbc326ffaf9ac3b0486679569d6de
by deone <playbydeone@gmail.com>
2018-10-17 11:13:33 +0100

    Création de deux autres fichiers de mon choix

---
+++ monessaijs.js
@@ -0,0 +1,6 @@
+var prenom = prompt("Entrez votre prénom :");
+var nom = prompt("Entrez votre nom :");
+alert("Bonjour, " + prenom + " " + nom);
+var age = Number(prompt("Quel est votre age :"));
+alert("Vous avez " + age + " ans");
+
